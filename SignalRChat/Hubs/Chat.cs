6068a9320ab4f7a2b9b7f776b0ee44d7bf47cd1b
Georgi Manov <georgimanov@gmail.com>
2017-01-23 00:27:07 +0200

    initial commit

---
+++ SignalRChat/Hubs/Chat.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace SignalRChat.Hubs
+{
+    public class Chat : Hub
+    {
+        public void SendMessage(string message)
+        {
+            var msg = string.Format("{0}", message);
+            Clients.All.addMessage(msg);
+        }
+
+        public void JoinRoom(string room)
+        {
+            Groups.Add(Context.ConnectionId, room);
+            Clients.Caller.joinRoom(room);
+        }
+
+        public void SendMessageToRoom(string message, string[] rooms)
+        {
+            var msg = string.Format("{0}", message);
+
+            for (int i = 0; i < rooms.Length; i++)
+            {
+                Clients.Group(rooms[i]).addMessage(msg);
+            }
+        }
+    }
+}
